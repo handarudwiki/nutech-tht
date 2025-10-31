8d5d30a807b22ca03c3da481395f775416195436
Handaru Dwiki <handarudwiki04@gmail.com>
2025-10-31 22:12:02 +0700

    use connection string

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,11 +5,13 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER ,
-  host: process.env.DB_HOST ,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DB_CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false },
+  // user: process.env.DB_USER ,
+  // host: process.env.DB_HOST ,
+  // port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASSWORD,
 });
 
 export default pool;
